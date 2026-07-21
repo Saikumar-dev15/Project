cc18322ce2fb773561bbba3f8521a4fb5001ca6d
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-07-21 21:30:10 +0530

    Coverting Str to int columns

--- Churn.py
+++ Churn.py
@@ -24,21 +24,63 @@ df = pd.read_csv("Churn.csv")
 #print(df.isnull())
 #print(df.value_counts())
 
-sns.histplot(data= df, x="gender" ,color = "blue", multiple="stack")
-plt.xlabel("gender")
-plt.show()
 
-sns.countplot( x="Churn",data=df,  hue="Churn", palette="Set2")
-plt.show()
+df["Churn"] = df["Churn"].map({
+    "No": 0,
+    "Yes": 1
+})
 
-sns.countplot(x="Contract", data=df, color="green",
+print("Churn")
+
+df["Contract"] = df["Contract"].map({
+    "Month-to-month": 0,
+    "One year": 1,
+    "Two year": 2
+})
+
+#print(df["Contract"])
+
+
+df["InternetService"] = df["InternetService"].map({
+    "DSL" : 0,
+    "Fiber optic": 1,
+    "No": 2
+})
+print(df["InternetService"])
+
+df["PaymentMethod"] = df["PaymentMethod"].map({
+    "Electronic check" : 0,
+   "Bank transfer (automatic)": 1,
+   "Credit card (automatic)": 2,
+    "Mailed check": 3
+})
+print(df["PaymentMethod"])
+
+df["InternetService"] = df["InternetService"].map({
+    "DSL" : 0,
+    "Fiber optic": 1,
+    "No": 2
+})
+print(df["InternetService"])
+
+
+
+
+#sns.histplot(data= df, x="gender" ,color = "blue", multiple="stack")
+#plt.xlabel("gender")
+#plt.show()
+
+#sns.countplot( x="Churn",data=df,  hue="Churn", palette="Set2")
+#plt.show()
+
+sns.countplot(x="PaymentMethod", data=df, color="green",
                                             edgecolor="black")
 plt.show()
 
-sns.boxplot(x="Churn", y="MonthlyCharges", data=df)
-plt.title("Monthly Charges by Churn")
-plt.show()
+#sns.boxplot(x="Churn", y="MonthlyCharges", data=df)
+#plt.title("Monthly Charges by Churn")
+#plt.show()
 
 
-sns.pairplot(df[["tenure", "MonthlyCharges", "TotalCharges", "Churn"]])
-plt.show()
+#sns.pairplot(df[["tenure", "MonthlyCharges", "TotalCharges", "Churn"]])
+#plt.show()
