2406aa8a6c27d68914a0229adcf19441ad55928d
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-08-12 01:34:09 +0530

    Implemented Confusion Matrix Visualization & precision recall curve using SK Logistic model

--- Projects/Churn.py
+++ Projects/Churn.py
@@ -122,14 +122,23 @@ end = time.time()
 sk_time = end - start
 print(f"Time to Predict Sk Logestic Model: {sk_time}")
 
-print("Recall Score: ", recall_score(y_test, y_pred))
-print("Roc Aug Score: ", roc_auc_score(y_test, y_prob_sk))
-print("Accuracy Score: ", accuracy_score(y_test, y_pred))
-print(f"Precision Score: {precision_score(y_test, y_pred)}")
-print("f1 Score: ", f1_score(y_test, y_pred))
-print(f"Precision Recall Curve: {precision_recall_curve(y_test, y_pred)}")
-print(f"PR- AUC Score: {average_precision_score(y_test, y_prob_sk)}")
-print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
+recall = recall_score(y_test, y_pred)
+print("Recall Score: ", recall)
+rocscore = roc_auc_score(y_test, y_prob_sk) 
+print("Roc Aug Score: ", rocscore)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy Score: ", accuracy)
+precision = precision_score(y_test, y_pred)
+print(f"Precision Score: {precision}")
+f1 = f1_score(y_test, y_pred)
+print("f1 Score: ", f1)
+precision_recall = precision_recall_curve(y_test, y_pred)
+print(f"Precision Recall Curve: {precision_recall}")
+average_precision = average_precision_score(y_test, y_prob_sk)
+print(f"PR- AUC Score: {average_precision}")
+print("------------------------ Confusion Matrix -------------------------------------------")
+print(confusion_matrix(y_test, y_pred))
+print("--------------------------------------------------------------------------------------")
 
 
 #Numpy Logistic Regression
@@ -211,24 +220,34 @@ np_time = end_num - start_num
         
 
 print("Training Time Taken by Numpy Logistic:",np_time)
-print("Accuracy of Numpy Logistic:",accuracy_score(y_test,np_pred))
-print("Precision Score of Numpy Logistic:",precision_score(y_test,np_pred))
-print("Recall Score of Numpy Logistic  :",recall_score(y_test,np_pred))
-print("F1 Score Score of Numpy Logistic:",f1_score(y_test,np_pred))
-print("ROC AUC  Score of Numpy Logistic:",roc_auc_score(y_test,np_pred_prob))
-print(f"Precision Recall Curve: {precision_recall_curve(y_test, np_pred)}")
-print(f"PR- AUC of Numpy Logistic: {average_precision_score(y_test, np_pred_prob)}")
-print("Confusion matrix :", confusion_matrix(y_test, np_pred))
+accuracy = accuracy_score(y_test,np_pred)
+print("Accuracy of Numpy Logistic:" , accuracy)
+precision = precision_score(y_test,np_pred)
+print("Precision Score of Numpy Logistic:",precision)
+recall = recall_score(y_test,np_pred)
+print("Recall Score of Numpy Logistic  :",recall)
+f1 = f1_score(y_test,np_pred)
+print("F1 Score Score of Numpy Logistic:",f1)
+roc_auc = roc_auc_score(y_test,np_pred_prob)
+print("ROC AUC  Score of Numpy Logistic:",roc_auc)
+precision_recall = precision_recall_curve(y_test, np_pred_prob)
+print(f"Precision Recall Curve: {precision_recall}")
+average_precision = average_precision_score(y_test, np_pred_prob)
+print(f"PR- AUC of Numpy Logistic: {average_precision}")
+print("------------------------ Confusion Matrix -------------------------------------------")
+print(confusion_matrix(y_test, np_pred))
+print("--------------------------------------------------------------------------------------")
+
 
 sns.histplot(data= df, x="gender" ,color = "#87cbf5", multiple="stack")
 plt.title("Category Based Members")
 plt.xlabel("Gender")
 plt.ylabel("Total")
-plt.show()
+#plt.show()
 
 sns.countplot( x="Churn",data=df,  hue="Churn", palette="Set2")
 plt.title("CHURN")
-plt.show()
+#plt.show()
 
 
 sns.countplot(x="InternetService", data=df, color="#edb940",
@@ -236,13 +255,13 @@ sns.countplot(x="InternetService", data=df, color="#edb940",
 plt.title("INTERNET SERVICE")
 plt.xlabel("Services")
 plt.ylabel("Total")
-plt.show()
+#plt.show()
 
 sns.countplot(x="DeviceProtection", data=df, color="#92f467e8",
                                             edgecolor="black")
 plt.title("DEVICE PROTECTION")
 plt.xlabel("Devices")
-plt.show()
+#plt.show()
 
 
 #Hyperparameter
@@ -267,8 +286,8 @@ grid.fit(X_train, y_train)
 
 grid_model = grid.predict(X_test)
 
-print("Best Parameters: ", grid.best_params_)
-print("Best Accuracy: ", grid.best_score_)
+#print("Best Parameters: ", grid.best_params_)
+#print("Best Accuracy: ", grid.best_score_)
 
 best_model = grid.best_estimator_
 
@@ -278,16 +297,44 @@ end_grid = time.time()
 
 time_grid = end_grid - start_grid
 
-print(f"Time take by the Grid model: {time_grid}")
-print(f"Test Accuracy score of Tuned Model: {accuracy_score(y_test, y_pred_Grid)}")
-print(f"Test Precision score of Tuned Model: {precision_score(y_test, y_pred_Grid)}")
-print(f"Test Recall score of Tuned Model: {recall_score(y_test, y_pred_Grid)}")
-print(f"Test F1 score of Tuned Model: {f1_score(y_test, y_pred_Grid)}")
-print(f"Test Average precision score of Tuned Model: {average_precision_score(y_test, y_pred_prob_grid)}")
-print(f"Test Roc AUC Score of Tuned Model: {roc_auc_score(y_test, y_pred_prob_grid)}")
+#print(f"Time take by the Grid model: {time_grid}")
+#print(f"Test Accuracy score of Tuned Model: {accuracy_score(y_test, y_pred_Grid)}")
+#print(f"Test Precision score of Tuned Model: {precision_score(y_test, y_pred_Grid)}")
+#print(f"Test Recall score of Tuned Model: {recall_score(y_test, y_pred_Grid)}")
+#print(f"Test F1 score of Tuned Model: {f1_score(y_test, y_pred_Grid)}")
+#print(f"Precision Recall Curve of Tuned Model: {precision_recall_curve(y_test, y_pred_prob_grid)}")
+#print(f"Test Average precision score of Tuned Model: {average_precision_score(y_test, y_pred_prob_grid)}")
+#print(f"Test Roc AUC Score of Tuned Model: {roc_auc_score(y_test, y_pred_prob_grid)}")
 
 corr = df.corr(numeric_only=True)
-print(corr)
+#print(corr)
 
-sns.heatmap(corr, annot=True)
-plt.show()
+#sns.heatmap(corr, annot=True)
+#plt.show(block=True)
+
+
+cm = confusion_matrix(y_test, y_pred)
+
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt="d",
+    xticklabels=["No Churn", "Churn"],
+    yticklabels=["No Churn", "Churn"]
+)
+
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix Of SK Model")
+#plt.show()
+
+
+precision, recall, thresholds = precision_recall_curve(
+    y_test,
+    y_prob_sk
+)
+plt.plot(recall, precision)
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.title("Precision-Recall Curve")
+#plt.show(block=True)
