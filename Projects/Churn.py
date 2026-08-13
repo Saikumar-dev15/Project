042f7b31fb143aa841e4fc568d7dcc30214ffbd0
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-08-13 11:28:53 +0530

    Implemented a Report that analyse the Risk level that identify churn and contact him for further updates...

--- Projects/Churn.py
+++ Projects/Churn.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time 
+import warnings
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.compose import ColumnTransformer
@@ -24,7 +26,7 @@ from sklearn.metrics import (
 
 df = pd.read_csv("Projects/Churn.csv")
 
-df = df.drop("customerID", axis=1)
+
 #print(df)
 
 #print(df.head(5))
@@ -103,14 +105,6 @@ X_test = preprocessor.transform(x_test)
 #print("Infinity in x_train:", np.isinf(x_train).sum())
 #print("Infinity in x_test:", np.isinf(x_test).sum())
 
-X_train = np.nan_to_num(X_train,
-                        nan=0.0,
-                        posinf=0.0,
-                        neginf=0.0)
-
-X_test = np.nan_to_num(X_test , nan=0.0,
-                       posinf=0.0,
-                       neginf=0.0)
 
 start = time.time()
 model = LogisticRegression( class_weight="balanced",max_iter=1000)
@@ -120,22 +114,14 @@ y_prob_sk = model.predict_proba(X_test)[:,1]
 end = time.time()
 
 sk_time = end - start
-print(f"Time to Predict Sk Logestic Model: {sk_time}")
-
-recall = recall_score(y_test, y_pred)
-print("Recall Score: ", recall)
+recall_sk = recall_score(y_test, y_pred)
 rocscore = roc_auc_score(y_test, y_prob_sk) 
-print("Roc Aug Score: ", rocscore)
 accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy Score: ", accuracy)
-precision = precision_score(y_test, y_pred)
-print(f"Precision Score: {precision}")
+precision_sk = precision_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
-print("f1 Score: ", f1)
-precision_recall = precision_recall_curve(y_test, y_pred)
+precision_recall = precision_recall_curve(y_test, y_prob_sk)
 print(f"Precision Recall Curve: {precision_recall}")
 average_precision = average_precision_score(y_test, y_prob_sk)
-print(f"PR- AUC Score: {average_precision}")
 print("------------------------ Confusion Matrix -------------------------------------------")
 print(confusion_matrix(y_test, y_pred))
 print("--------------------------------------------------------------------------------------")
@@ -219,21 +205,14 @@ np_pred_prob = scratch.predict_probability(X_test)
 np_time = end_num - start_num
         
 
-print("Training Time Taken by Numpy Logistic:",np_time)
 accuracy_num = accuracy_score(y_test,np_pred)
-print("Accuracy of Numpy Logistic:" , accuracy_num)
 precision_num = precision_score(y_test,np_pred)
-print("Precision Score of Numpy Logistic:",precision_num)
 recall_num = recall_score(y_test,np_pred)
-print("Recall Score of Numpy Logistic  :",recall_num)
 f1_num = f1_score(y_test,np_pred)
-print("F1 Score Score of Numpy Logistic:",f1_num)
 roc_auc_num = roc_auc_score(y_test,np_pred_prob)
-print("ROC AUC  Score of Numpy Logistic:",roc_auc_num)
 precision_recall_num = precision_recall_curve(y_test, np_pred_prob)
 print(f"Precision Recall Curve: {precision_recall_num}")
 average_precision_num = average_precision_score(y_test, np_pred_prob)
-print(f"PR- AUC of Numpy Logistic: {average_precision_num}")
 print("------------------------ Confusion Matrix -------------------------------------------")
 print(confusion_matrix(y_test, np_pred))
 print("--------------------------------------------------------------------------------------")
@@ -243,7 +222,7 @@ sns.histplot(data= df, x="gender" ,color = "#87cbf5", multiple="stack")
 plt.title("Category Based Members")
 plt.xlabel("Gender")
 plt.ylabel("Total")
-#plt.show()
+plt.show()
 
 sns.countplot( x="Churn",data=df,  hue="Churn", palette="Set2")
 plt.title("CHURN")
@@ -262,22 +241,22 @@ sns.countplot(x="InternetService", data=df, color="#edb940",
 plt.title("INTERNET SERVICE")
 plt.xlabel("Services")
 plt.ylabel("Total")
-#plt.show()
+plt.show()
 
 sns.countplot(x="DeviceProtection", data=df, color="#92f467e8",
                                             edgecolor="black")
 plt.title("DEVICE PROTECTION")
 plt.xlabel("Devices")
-#plt.show()
+plt.show()
 
 
 #Hyperparameter
-start_grid = time.time()
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 model_1 = LogisticRegression(random_state=42)
 
 param_grid = {
-    'C' : [0.01, 0.1, 1, 10, 100],
-    'solver': ['lbfgs', 'liblinear'],
+     'C': [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100],
+    'solver': ['lbfgs', 'liblinear', 'saga'],
     'class_weight' : [None, 'balanced']
 }
 
@@ -285,16 +264,17 @@ grid = GridSearchCV(
     estimator = model_1,
     param_grid =param_grid,
     cv= 5,
-    scoring = 'f1'
+    scoring = 'f1',
+    refit=True
 )
-
+start_grid = time.time()
 
 grid.fit(X_train, y_train)
 
 grid_model = grid.predict(X_test)
 
-#print("Best Parameters: ", grid.best_params_)
-#print("Best Accuracy: ", grid.best_score_)
+print("Best Parameters: ", grid.best_params_)
+print("Best Accuracy: ", grid.best_score_)
 
 best_model = grid.best_estimator_
 
@@ -304,27 +284,23 @@ end_grid = time.time()
 
 time_grid = end_grid - start_grid
 
-#print(f"Time take by the Grid model: {time_grid}")
 accuracy_grid = accuracy_score(y_test, y_pred_Grid)
-print(f"Test Accuracy score of Tuned Model: {accuracy_grid}")
 precision_grid = precision_score(y_test, y_pred_Grid)
-print(f"Test Precision score of Tuned Model: {precision_grid}")
 recall_grid = recall_score(y_test, y_pred_Grid)
-print(f"Test Recall score of Tuned Model: {recall_grid}")
 f1_grid = f1_score(y_test, y_pred_Grid)
-print(f"Test F1 score of Tuned Model: {f1_grid}")
+average_precision_grid = average_precision_score(y_test, y_pred_prob_grid)
+
 precision_recall_grid = precision_recall_curve(y_test, y_pred_prob_grid)
 print(f"Precision Recall Curve of Tuned Model: {precision_recall_grid}")
-average_precision_grid = average_precision_score(y_test, y_pred_prob_grid)
-print(f"Test Average precision score of Tuned Model: {average_precision_grid}")
 roc_auc_grid = roc_auc_score(y_test, y_pred_prob_grid)
 print(f"Test Roc AUC Score of Tuned Model: {roc_auc_grid}")
 
 corr = df.corr(numeric_only=True)
-#print(corr)
+print(corr)
 
-#sns.heatmap(corr, annot=True)
-#plt.show(block=True)
+sns.heatmap(corr, annot=True)
+plt.title("HeatMap of Numerical Columns")
+plt.show(block=True)
 
 
 cm = confusion_matrix(y_test, y_pred)
@@ -340,7 +316,7 @@ sns.heatmap(
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix Of SK Model")
-#plt.show()
+plt.show()
 
 
 precision, recall, thresholds = precision_recall_curve(
@@ -351,4 +327,77 @@ plt.plot(recall, precision)
 plt.xlabel("Recall")
 plt.ylabel("Precision")
 plt.title("Precision-Recall Curve")
-#plt.show(block=True)
+plt.show(block=True)
+
+
+results = pd.DataFrame({
+    "Models"            : ["SK Logistic", "Num Logistic", "Grid Model"],
+    "Time"              : [sk_time, np_time, time_grid],
+    "Recall"            : [recall_sk, recall_num , recall_grid],
+    "Accuracy"          : [accuracy, accuracy_num, accuracy_grid ],
+    "Precision"         : [precision_sk , precision_num , precision_grid ],
+    "F1"                : [f1, f1_num, f1_grid],
+    "ROC"               : [rocscore, roc_auc_num, roc_auc_grid],
+    "Average Precision" : [average_precision, average_precision_num, average_precision_grid]
+})
+
+print("*********************************************** REPORT *******************************************************************************")
+
+print(results)
+
+
+customer_churn_probability = best_model.predict_proba(X_test)[:, 1]
+
+def risk_level(probability):                                          #risk levels predictions
+    if probability >= 0.70:
+        return "High Risk"
+    elif probability >= 0.40:
+        return "Medium Risk"
+    else :
+        return "Low Risk"
+    
+
+risk = [risk_level(p) for p in customer_churn_probability]
+
+def recommendations(risk):
+    if risk == "High Risk":
+        return "Immediate retention offer"
+    elif risk == "Medium Risk":
+        return "Monitor and Contact for further deals"
+    else : 
+        return "Normal Customer"
+    
+    
+actions = [recommendations(r) for r in risk]
+
+
+
+customer_risk_report = x_test.copy()
+
+customer_risk_report["Churn Probability"]  = customer_churn_probability
+customer_risk_report["Risk Level"]         = risk
+customer_risk_report["Recommended Action"] = actions
+
+
+
+customer_risk_report = customer_risk_report[
+    ["customerID",
+     "tenure",
+     "MonthlyCharges",
+     "TotalCharges",
+     "Contract",
+     "InternetService",
+     "Churn Probability",
+     "Risk Level",
+     "Recommended Action"
+    ]
+]
+
+#Sort the highest Risk LEvel
+customer_risk_report = customer_risk_report.sort_values(
+    by="Churn Probability",
+    ascending = False
+)
+
+print("******************************** Customer Risk Report************************************************")
+print(customer_risk_report.head(25))
